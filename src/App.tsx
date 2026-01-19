e001caa6b6aa6ca79851cb8c4f4b6e5f51fc3d62
yongteck <yoinkyteck@gmail.com>
2026-01-19 22:46:29 +0800

    landing page made

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="absolute inset-0 -z-10">
+        <div className="aspect-[16/9] h-auto w-full bg-[url('https://th.bing.com/th/id/R.b0f6a3b05df23ee73cf4d8d16ca38221?rik=q3aHizibej2b0w&riu=http%3a%2f%2fwonderfulengineering.com%2fwp-content%2fuploads%2f2014%2f04%2fspace-wallpaper-2.jpg&ehk=eCnmaCABoV11k44qdKr4kJyGksNvdTbUDQQaprEsZlY%3d&risl=&pid=ImgRaw&r=0')] bg-cover bg-center"></div>
+        <div className="aspect-[16/9] h-auto w-full bg-[url('https://i.pinimg.com/originals/21/08/6d/21086dbd5783d15f4b4d255ae5bba4b7.jpg')] bg-cover bg-center"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="grid w-full grid-cols-[1fr_3.5fr_1fr] grid-rows-[15px_50px] gap-2 px-5 pb-2">
+        <div className="col-span-3"></div>
+        <div className="flex place-items-center justify-center">
+          <img
+            className="h-full w-auto"
+            src="https://springblasters.com/cdn/shop/files/DFE4AF64-E6CE-4C82-A3C8-3821FCDD7AF5.jpg?v=1613640668&width=240"
+          />
+        </div>
+
+        <nav className="col-span-1 flex flex-wrap justify-center-safe gap-x-4 gap-y-1 pe-5">
+          <div className="nav-item">About Us</div>
+          <div className="nav-item">Services</div>
+          <div className="nav-item">Industries</div>
+          <div className="nav-item">Resources</div>
+          <div className="nav-item">Partners</div>
+        </nav>
+
+        <div className="flex flex-wrap place-items-center justify-center gap-5">
+          <div className="rounded-2xl bg-amber-500 py-1 px-5 text-center font-semibold text-[#FFFFFF]">
+            Contact us
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="grid w-full aspect-[20/9] grid-cols-[Repeat(21,1fr)] grid-rows-[5vw_2fr_3fr_1fr]">
+        <div className="[grid-area:2/2/3/8]">
+          <h1 className="text-2xl text-white font-semibold">
+            The place with the highest amount of trees
+          </h1>
+          <div className="text-lg text-white font-semibold">
+            Although diamonds and other rare earths are considered to be the
+            rarest resources, it surprisingly is not. In the scale of the
+            universe, wood is the truely rarest resource, only found on
+            Earth.{" "}
+          </div>
+        </div>
+
+        <div className="[grid-area:3/16/4/21] flex flex-col">
+          <h1 className="text-2xl text-white font-semibold text-center">
+            A wet place
+          </h1>
+          <div className="text-lg text-white font-semibold text-center">
+            {" "}
+            Almost all of Earth's water is contained in its global ocean,
+            covering 70.8% of Earth's crust. The remaining 29.2% of Earth's
+            crust is land, most of which is located in the form of continental
+            landmasses within Earth's land hemisphere.{" "}
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="bg-blue-300"> break line </div>  */}
+
+      <div className="w-full h-auto aspect-[20/9] grid grid-cols-[5vw_1fr_2fr_10vw] grid-rows-[15vw_20vw_1fr] p-4">
+        <div className="col-span-4">stuff</div>
+        <div className="[grid-area:2/2] flex flex-col">
+          <h1 className="text-2xl text-white font-semibold text-center">
+            The Red planet of minerals
+          </h1>
+          <div className="text-lg text-white font-semibold text-center">
+            Mars is the fourth planet from the Sun. It is also known as the "Red
+            Planet", for its orange-red appearance.Mars is a desert-like rocky
+            planet with a tenuous atmosphere that is primarily carbon dioxide
+            (CO2). At the average surface level the atmospheric pressure is a
+            few thousandths of Earth's, atmospheric temperature ranges from -153
+            to 20 °C, and cosmic radiation is high.
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
